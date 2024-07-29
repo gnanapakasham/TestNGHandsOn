eb6fcce24dd009e3c7de7a86891cd0e500766773
Gnanaprakasham S <sgprakash247@gmail.com>
2024-07-29 14:17:54 +0530

    TestNG handson

--- src/test/java/testng/test/TestNGParamemeterization.java
+++ src/test/java/testng/test/TestNGParamemeterization.java
@@ -1,5 +1,6 @@
 package testng.test;
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -7,7 +8,7 @@ public class TestNGParamemeterization {
 
 	@Parameters("keyword")
 	@Test(description = "Method to do search in browser engine")
-	public void searchEngine(String keyword) {
+	public void searchEngine(@Optional String keyword) {
 		System.out.println("Searching from " + keyword);
 	}
 
